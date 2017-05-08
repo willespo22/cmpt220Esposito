30edffc438e6e3c813cfbfbc697c508545db4e7e
willespo22 <willespo22@yahoo.com>
2017-05-08 19:22:33 -0400

    Update Problem2_1.java
    styling + header

--- labs/1/src/Problem2_1.java
+++ labs/1/src/Problem2_1.java
@@ -5,17 +5,17 @@
 import java.util.Scanner; //Scanner is in the java.util package
 
 public class Problem2_1 {
-	public static void main(String[] args) {
-		// Create a Scanner object
-		Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    // Create a Scanner object
+    Scanner input = new Scanner(System.in);
+ 
+    // Prompt the user to enter a degree in Celcius
+    System.out.print("Enter a degree in Celcius: ");
+    double celcius = 0, farenheit = input.nextDouble();
 
-		// Prompt the user to enter a degree in Celcius
-		System.out.print("Enter a degree in Celcius: ");
-		double celcius = 0, farenheit = input.nextDouble();
+     // Compute temperature
+     farenheit = (9.0 / 5) * celcius + 32;
 
-		// Compute temperature
-		farenheit = (9.0 / 5) * celcius + 32;
-
-		System.out.println(celcius + " celcius is " + farenheit + " degrees Farenheit.");
-	}
+     System.out.println(celcius + " celcius is " + farenheit + " degrees Farenheit.");
+  }
 }
