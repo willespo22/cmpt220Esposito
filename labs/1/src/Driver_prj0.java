18aed4a70a1ea562ead4729f5955981825c53523
willespo22 <willespo22@yahoo.com>
2017-05-08 19:24:59 -0400

    Update Driver_prj0.java
    header

--- labs/1/src/Driver_prj0.java
+++ labs/1/src/Driver_prj0.java
@@ -1,3 +1,15 @@
+/**
+ * file: Driver_prj0.java 
+ * author: William Esposito 
+ * course: CMPT 220 
+ * assignment: Lab 1
+ * due date: January 26
+ * version: 1.3
+ * 
+ * This file contains a program with answer for designated problem
+ */
+
+
 public class Driver_prj0 {
   public static void main(String[] args) {
     System.out.println("      J     A      V     V     A");
